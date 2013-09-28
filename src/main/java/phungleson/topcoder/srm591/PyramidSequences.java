2c2868b5da722a88d36a2209537d1393738ec8bd
Son <leson.phung@gmail.com>
2013-09-28 09:41:15 +0800

    Bruce force way to solve pyramid sequences

--- src/main/java/phungleson/topcoder/srm591/PyramidSequences.java
+++ src/main/java/phungleson/topcoder/srm591/PyramidSequences.java
@@ -1,14 +1,74 @@
 package phungleson.topcoder.srm591;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * http://community.topcoder.com/stat?c=problem_statement&pm=12619
  * 
  * @author son
  */
 public class PyramidSequences {
-	public long distinctPairs(int n, int m) {
-		// (2n - 1) * s1
-		// (2m - 1) * s2
-		return 0;
+
+	public static class Pair {
+		public Pair(int n, int m) {
+			this.n = n;
+			this.m = m;
+		}
+
+		int n;
+
+		int m;
+	}
+
+	public long distinctPairs(int nMax, int mMax) {
+		int nValue;
+		int nPreviousValue = 0;
+		int nInc = 1;
+		int mValue;
+		int mPreviousValue = 0;
+		int mInc = 1;
+		List<Pair> pairs = new ArrayList<>();
+
+		while (true) {
+			nValue = nPreviousValue + nInc;
+			if (nValue == nMax) {
+				nInc = -1;
+			}
+			if (nValue == 1) {
+				nInc = +1;
+			}
+
+			mValue = mPreviousValue + mInc;
+			if (mValue == mMax) {
+				mInc = -1;
+			}
+			if (mValue == 1) {
+				mInc = +1;
+			}
+
+			boolean exists = false;
+
+			// for (Pair pair : pairs) {
+			// if (pair.n == nValue && pair.m == mValue) {
+			// exists = true;
+			// break;
+			// }
+			// }
+
+			// if (!exists) {
+			// pairs.add(new Pair(nValue, mValue));
+			// }
+
+			if ((nValue == nMax && mValue == 1) ||
+				(nValue == 1 && mValue == mMax) ||
+				(nValue == nMax && mValue == mMax)) {
+				break;
+			}
+			nPreviousValue = nValue;
+			mPreviousValue = mValue;
+		}
+
+		return pairs.size();
 	}
 }
--- src/test/java/phungleson/topcoder/srm591/PyramidSequences.spec
+++ src/test/java/phungleson/topcoder/srm591/PyramidSequences.spec
@@ -3,8 +3,11 @@ package phungleson.topcoder.srm591
 describe PyramidSequences {
 	describe "#distinctPairs" {
 		def tests {
-			|	a 	|	b	|	c	|
-			|	1	|	2	|	3	|
+			|	a 	|	b			|	c			|
+			|	3	|	4			|	6l			|
+			|	3	|	5			|	5l			|
+			|	43	|	76			|	895l		|
+			|	2	|	1000000000	|	1000000000l	|
   		}
  
 		fact tests.forEach[subject.distinctPairs(a, b) => c]
